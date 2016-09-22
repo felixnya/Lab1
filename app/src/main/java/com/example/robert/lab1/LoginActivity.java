1bb1ce889de6843c2d577efc60ef3f42ca672f5b
Robert <felixnya@gmail.com>
2016-09-22 14:08:52 -0400

    default

--- app/src/main/java/com/example/robert/lab1/LoginActivity.java
+++ app/src/main/java/com/example/robert/lab1/LoginActivity.java
@@ -1,23 +1,23 @@
 package com.example.robert.lab1;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class StartActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_login);
 
-        Button next = (Button) findViewById(R.id.button2);
+        Button next = (Button) findViewById(R.id.button3);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent ffs = new Intent(getApplicationContext(),LoginActivity.class );
+                Intent ffs = new Intent(getApplicationContext(),ListItemsActivity.class );
                 startActivity(ffs);
             }
         });
